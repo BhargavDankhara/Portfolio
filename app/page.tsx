86ac9a5b5b64414630c590d4fc4f361dd4c043cb
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-05-27 18:11:16 +0530

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <h1>HELLO</h1>
+        <h1>HELLO world</h1>
       </div>
     </main>
   );
