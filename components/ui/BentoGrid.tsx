449ececa62ef232820fa7ef01e6c1baef07891df
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-05-29 16:50:52 +0530

    unwanted div omited!

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -81,10 +81,7 @@ export const BentoGridItem = ({
                     )}
                 </div>
                 {id === 6 && (
-                    <BackgroundGradientAnimation>
-                        <div className="absolute z-50 flex items-center justify-center text-white font-bold">
-                        </div>
-                    </BackgroundGradientAnimation>
+                    <BackgroundGradientAnimation />
                 )}
 
                 <div className={cn(
