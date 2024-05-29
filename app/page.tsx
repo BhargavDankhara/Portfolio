8083e30c0d2788a10f2b2cc03ea915d079ff8a98
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-05-29 23:09:36 +0530

    Footer Section Done! and fixed some ui bugs

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import Approach from "@/components/Approach";
 import Clients from "@/components/Clients";
 import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
@@ -10,7 +11,7 @@ import { FaHome } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
@@ -19,6 +20,7 @@ export default function Home() {
         <Clients />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   );
---
+++ components/Footer.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import MagicButton from './ui/MagicButton'
+import { FaLocationArrow } from 'react-icons/fa6'
+import { socialMedia } from '@/data'
+
+const Footer = () => {
+    return (
+        <footer className='w-full mb-[100px] md:mb-5 pb-10' id='contact'>
+            
+            <div className='flex flex-col items-center'>
+                <h1 className='heading lg:max-w-[45vw]'>
+                    Ready to take <span className='text-purple'>Your</span> digital presence to te next level?
+                </h1>
+                <p className='text-white-200 md:mt-10 my-5 text-center'>Reach out to me today and let&apos;s  discuss how i can help you achive your goals.</p>
+                <a href="mailto:bhargavdankhara17@gmail.com">
+                    <MagicButton 
+                        title='Let&apos;s get in touch'
+                        icon={<FaLocationArrow />}
+                        position='right'
+                    />
+                </a>
+            </div>
+            <div className='flex mt-16 md:flex-row flex-col justify-between items-center'>
+                <p className='md:text-base text-sm md:font-normal font-light'>Copyright © 2024 Bhargav Dankhara</p>
+                <div className='flex items-center md:gap-3 gap-6'>
+                    {socialMedia.map((profile) => (
+                        <div key={profile.id} className='w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 border border-black-300 rounded-lg'>
+                            <img src={profile.img} alt="social media" 
+                            width={20} height={20}
+                            />
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
