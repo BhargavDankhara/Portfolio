7f20878c8abbef0a0c19bbf756d525edca357fec
BhargavDankhara <154862656+BhargavDankhara@users.noreply.github.com>
2024-05-29 18:48:51 +0530

    sentry setup

---
+++ sentry.server.config.ts
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://4fe65da7de8b70a1465ab79528ceeabb@o4507339739103232.ingest.us.sentry.io/4507339740676096",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
